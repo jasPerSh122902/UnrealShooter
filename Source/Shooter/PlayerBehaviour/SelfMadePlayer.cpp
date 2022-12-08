03e3c38121b3bd5f8802c3c7f78ed37f37f0e684
jasper <jasperjules1229@yahoo.com>
2022-12-08 16:54:19 -0600

    made a charactor spawn in and stand there and input is being taken in

--- Source/Shooter/PlayerBehaviour/SelfMadePlayer.cpp
+++ Source/Shooter/PlayerBehaviour/SelfMadePlayer.cpp
@@ -1,14 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PlayerBehaviour/SelfMadePlayer.h"
+#include <Shooter/PlayerBehaviour/SelfMadePlayer.h>
+#include <Camera/CameraComponent.h>
+#include <Components/CapsuleComponent.h>
 
 // Sets default values
 ASelfMadePlayer::ASelfMadePlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
 
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
+	// Create a CameraComponent	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(GetCapsuleComponent());
+	CameraComponent->SetRelativeLocation(CamLocation); // Position the camera
+	CameraComponent->bUsePawnControlRotation = true;
 }
 
 // Called when the game starts or when spawned
@@ -25,10 +38,45 @@ void ASelfMadePlayer::Tick(float DeltaTime)
 
 }
 
+void ASelfMadePlayer::MoveX(float value)
+{
+	if (value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), value);
+	}
+}
+
+void ASelfMadePlayer::MoveY(float value)
+{
+	if (value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorForwardVector(), value);
+	}
+}
+
 // Called to bind functionality to input
 void ASelfMadePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	/// <summary>
+	/// Binds the jump to Space
+	/// </summary>
+	/// <param name="PlayerInputComponent"></param>
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	/// <summary>
+	/// Binds the axis movement to the WASD keys
+	/// </summary>
+	/// <param name="PlayerInputComponent"></param>
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASelfMadePlayer::MoveY);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASelfMadePlayer::MoveX);
+	/// <summary>
+	/// Binds the rotation of the charactor to the mouse
+	/// </summary>
+	/// <param name="PlayerInputComponent"></param>
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
--- Source/Shooter/PlayerBehaviour/SelfMadePlayer.h
+++ Source/Shooter/PlayerBehaviour/SelfMadePlayer.h
@@ -6,24 +6,58 @@
 #include "GameFramework/Character.h"
 #include "SelfMadePlayer.generated.h"
 
+class UCameraComponent;
+class USkeletalMeshComponent;
+
 UCLASS()
 class SHOOTER_API ASelfMadePlayer : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(VisibleDefaultsOnly, Category = " Mesh ")
+	USkeletalMeshComponent* MeshA;
+	/// <summary>
+	/// Camera
+	/// </summary>
+	UPROPERTY(VisibleAnywhere,  Category = " Camera " )
+	UCameraComponent* CameraComponent;
 public:
-	// Sets default values for this character's properties
+	/// </summary>
+	/// Sets default values for this character's properties
+	/// /// </summary>
 	ASelfMadePlayer();
-
 protected:
-	// Called when the game starts or when spawned
+	/// </summary>
+	/// Called when the game starts or when spawned
+	/// </summary>
 	virtual void BeginPlay() override;
-
 public:	
-	// Called every frame
+	/// </summary>
+	/// Called every frame
+	/// </summary>
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
+	/// <summary>
+	/// Move on the X
+	/// </summary>
+	/// <param name="value">is the input</param>
+	void MoveX(float value);
+	/// <summary>
+	/// Move on the Y
+	/// </summary>
+	/// <param name="value">is the input</param>
+	void MoveY(float value);
+	/// <summary>
+	///  Called to bind functionality to input
+	/// </summary>
+	/// <param name="PlayerInputComponent">input</param>
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere,  Category = "Camera")
+	float BaseLookUpRate;
+	UPROPERTY(VisibleAnywhere)
+	FVector CamLocation;
 };
