f0e4bcf001c50b8b3bced0213ec6a5af845702f7
jasper <jasperjules1229@yahoo.com>
2023-02-02 11:04:34 -0600

    editor dos not crash now, fixed the raycast(This was causing the crash).

--- Source/Shooter/Private/TraceComp.cpp
+++ Source/Shooter/Private/TraceComp.cpp
@@ -17,34 +17,36 @@ UTraceComp::UTraceComp()
 
 // Called when the game starts
 void UTraceComp::BeginPlay() {Super::BeginPlay();}
-void UTraceComp::DoTrace()
+void UTraceComp::DoTrace(FVector location, FRotator rotation,UWorld* currentWorld)
 {
-
+	FVector tempLocation = location;
+	FRotator temprotation = rotation;
+	FVector tempEndLocation;
 	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Emerald, "Trace");
-	// set the start line
-	LineStart = GetOwner()->GetActorLocation();
-	// set the rotation equal to the components rotation
-	Rotation = GetOwner()->FindComponentByClass<UCameraComponent>()->GetComponentRotation();
+	tempLocation = FVector(tempLocation.X + (temprotation.Vector().X * 100), tempLocation.Y + (temprotation.Vector().Y * 100), tempLocation.Z + (temprotation.Vector().Z * 100));
 	// the start will increate by the rotation
-	LineStart = FVector(LineStart.X + (Rotation.Vector().X * 100), LineStart.Y + (Rotation.Vector().Y * 100), LineStart.Z + (Rotation.Vector().Z * 100));
-	LineEnd = LineStart + (Rotation.Vector() * EndMultiply);
+	tempEndLocation = tempLocation + temprotation.Vector();
 	// Makes a hit result
 	FHitResult hit;
 	// If the world return true
-	if (GetWorld())
+	if (currentWorld != NULL)
 	{
 		// Makes a trace 
 		// retruns true if the trace hit
 		// else return false
-		bool Traced = GetWorld()->LineTraceSingleByChannel(hit, LineStart , LineEnd, ECC_PhysicsBody, FCollisionQueryParams(), FCollisionResponseParams());
+		bool Traced = currentWorld->LineTraceSingleByChannel(hit, tempLocation, tempEndLocation, ECC_PhysicsBody, FCollisionQueryParams(), FCollisionResponseParams());
 		// Is the size of the bullet that i spawn
-		GetTraceBullet(100, FColor::Orange, false, 1.5f, 0, 5.0f);
+		GetTraceBullet(100, FColor::Orange, false, 1.5f, 0, 5.0f,currentWorld,tempLocation,tempEndLocation,temprotation);
 		
 		/// <summary>
 		/// Is the on collision call
 		/// </summary>
 		if (Traced && hit.GetActor())
 		{
+			if (hit.GetActor()->GetFName() == "SelfMadePlayer_0")
+			{
+				return;
+			}
 			// Make a Frotator
 			FRotator hitrotation;
 			// Set the rotator 
@@ -59,12 +61,12 @@ void UTraceComp::DoTrace()
 // Called every frame
 void UTraceComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {Super::TickComponent(DeltaTime, TickType, ThisTickFunction);}
 
-void UTraceComp::GetTraceBullet(float multiplyLength,FColor color, bool linePresist,float lifeTime, int proity, float thickness)
+void UTraceComp::GetTraceBullet(float multiplyLength,FColor color, bool linePresist,float lifeTime, int proity, float thickness, UWorld* currentWorld, FVector location, FVector endLocation, FRotator rotation)
 {
 	// Prinst the message to the log
 	UE_LOG(LogTemp, Warning, TEXT("Sending ray Trace"))
 	// Is the lines end
-	LineEnd = LineStart + Rotation.Vector() * multiplyLength;
+	endLocation = location + rotation.Vector() * multiplyLength;
 	// Draws the line at the current location to the end
-	DrawDebugLine(GetWorld(),LineStart,LineEnd, color,false,lifeTime, proity, thickness);
+	DrawDebugLine(currentWorld, location, endLocation, color,false,lifeTime, proity, thickness);
 }
